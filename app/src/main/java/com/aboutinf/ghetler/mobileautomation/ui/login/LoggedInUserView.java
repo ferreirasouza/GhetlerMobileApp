26f46f1926eab8f8dcb47db6abc270aa9bd2d9e3
ferreirasouza <33432950+ferreirasouza@users.noreply.github.com>
2021-05-19 14:54:29 -0400

    Starting

--- app/src/main/java/com/aboutinf/ghetler/mobileautomation/ui/login/LoggedInUserView.java
+++ app/src/main/java/com/aboutinf/ghetler/mobileautomation/ui/login/LoggedInUserView.java
@@ -6,7 +6,6 @@ package com.aboutinf.ghetler.mobileautomation.ui.login;
 class LoggedInUserView {
     private String displayName;
     //... other data fields that may be accessible to the UI
-
     LoggedInUserView(String displayName) {
         this.displayName = displayName;
     }
